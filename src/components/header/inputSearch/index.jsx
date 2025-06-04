79d5324184ec660aed58e2d02d33164f8d9e671d
Vladyslav Smahin <vladislavsmagin1@gmail.com>
2025-06-04 17:04:20 +0300

    add burger menu

--- src/components/header/inputSearch/index.jsx
+++ src/components/header/inputSearch/index.jsx
@@ -2,7 +2,7 @@ import './style.scss'
 import {Link} from "react-router-dom";
 import {useEffect, useState} from "react";
 
-const InputSearch = ({className = '', onResultClick }) => {
+const InputSearch = ({className = '', onResultClick}) => {
     const [posts, setPosts] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
     const [searchResults, setSearchResults] = useState([]);
@@ -38,37 +38,37 @@ const InputSearch = ({className = '', onResultClick }) => {
     return (
         <div className='headerInputWrapper'>
 
-                            <input
-                                type="text"
-                                placeholder="Search by name"
-                                className={`headerInputInput ${className}`}
-                                value={searchTerm}
-                                onChange={(e) => setSearchTerm(e.target.value)}
-                                onFocus={() => setIsFocused(true)}
+            <input
+                type="text"
+                placeholder="Search by name"
+                className={`headerInputInput ${className}`}
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                onFocus={() => setIsFocused(true)}
+            />
+            {searchTerm.length > 0 && isFocused && (
+                <div className="searchResults">
+                    {searchTerm.length < 3 ? (
+                        <div className="searchResultItem">Please enter at least 3 characters</div>
+                    ) : searchResults.length === 0 ? (
+                        <div className="searchResultItem">No matches found</div>
+                    ) : (
+                        searchResults.slice(0, 10).map((post) => (
+                            <Link
+                                key={post._id}
+                                to={`/${post.category}/post/${post._id}`}
+                                className="searchResultItem"
+                                onClick={() => {
+                                    setSearchTerm('');
+                                    setIsFocused(false);
+                                    if (onResultClick) onResultClick();
+                                }}
+                                dangerouslySetInnerHTML={{__html: post.title || 'пусто'}}
                             />
-                            {searchTerm.length > 0 && isFocused && (
-                                <div className="searchResults">
-                                    {searchTerm.length < 3 ? (
-                                        <div className="searchResultItem">Please enter at least 3 characters</div>
-                                    ) : searchResults.length === 0 ? (
-                                        <div className="searchResultItem">No matches found</div>
-                                    ) : (
-                                        searchResults.slice(0, 10).map((post) => (
-                                            <Link
-                                                key={post._id}
-                                                to={`/${post.category}/post/${post._id}`}
-                                                className="searchResultItem"
-                                                onClick={() => {
-                                                    setSearchTerm('');
-                                                    setIsFocused(false);
-                                                    if (onResultClick) onResultClick();
-                                                }}
-                                                dangerouslySetInnerHTML={{ __html: post.title || 'пусто' }}
-                                            />
-                                        ))
-                                    )}
-                                </div>
-                            )}
+                        ))
+                    )}
+                </div>
+            )}
         </div>
     );
 };
