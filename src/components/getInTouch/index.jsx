3bb0c3ff318b7ad16748d48a4b51495614939c34
Vladyslav Smahin <vladislavsmagin1@gmail.com>
2025-06-16 15:59:08 +0300

    test form

--- src/components/getInTouch/index.jsx
+++ src/components/getInTouch/index.jsx
@@ -1,68 +1,85 @@
-import './style.scss'
+import { useState } from 'react';
+import './style.scss';
 import Divider from "../divider/index.jsx";
-const GetInTouch = ({className = ''}) => {
+
+const GetInTouch = ({ className = '' }) => {
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        message: '',
+    });
+
+    const handleChange = (e) => {
+        const { id, value } = e.target;
+        setFormData((prev) => ({ ...prev, [id]: value }));
+    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        const formData = {
-            firstName: e.target.firstName.value,
-            lastName: e.target.lastName.value,
-            email: e.target.email.value,
-            message: e.target.message.value,
-        };
+        try {
+            const res = await fetch("https://abc-back.onrender.com/contact", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
+            });
 
-        await fetch('https://abc-back.onrender.com/contact', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(formData),
-        });
-
-        alert('Message sent!');
+            if (res.ok) {
+                alert("Message sent!");
+                setFormData({ firstName: '', lastName: '', email: '', message: '' }); // Очистить форму
+            } else {
+                alert("Failed to send message");
+                const errorText = await res.text();
+                console.error("Server error:", errorText);
+            }
+        } catch (err) {
+            alert("Error sending message");
+            console.error("Network error:", err);
+        }
     };
-
 
     return (
         <div className={`getInTouch ${className}`} id='getInTouch'>
-            <Divider className='getInTouchDivider'/>
+            <Divider className='getInTouchDivider' />
             <div className='getInTouchContent container'>
                 <div className='getInTouchContent_leftColumn'>
                     <h3>Get in touch</h3>
-                    <p>If you have any questions or would you like to submit an article for consideration, feel free for
-                        reach out to us</p>
-                    <form className='getInTouchForm'  onSubmit={handleSubmit}>
+                    <p>If you have any questions or would you like to submit an article for consideration, feel free to reach out to us</p>
+                    <form className='getInTouchForm' onSubmit={handleSubmit}>
                         <div className='nameWrapper'>
                             <label htmlFor="firstName"></label>
-                            <input id="firstName" type="text" placeholder="Name"/>
+                            <input id="firstName" type="text" placeholder="Name" value={formData.firstName} onChange={handleChange} />
 
                             <label htmlFor="lastName"> </label>
-                            <input id="lastName" type="text" placeholder="Last Name"/>
+                            <input id="lastName" type="text" placeholder="Last Name" value={formData.lastName} onChange={handleChange} />
                         </div>
 
                         <label htmlFor="email"></label>
-                        <input id="email" type="email" placeholder="E-mail"/>
+                        <input id="email" type="email" placeholder="E-mail" value={formData.email} onChange={handleChange} />
 
                         <label htmlFor="message"></label>
-                        <textarea id="message" placeholder="Your Message"/>
-
+                        <textarea id="message" placeholder="Your Message" value={formData.message} onChange={handleChange} />
 
                         <button type='submit' className='getInTouchButton'>Send message</button>
                     </form>
                 </div>
-               {/* {isMobile && <Divider className='FollowUsDivider'/>}*/}
+
                 <div className='getInTouchContent_rightColumn'>
                     <h3>Follow Us</h3>
                     <div className='socialsContainer'>
                         <div className='socials'>
-                            <img src='/images/X-icon.svg' alt='socials'/>
+                            <img src='/images/X-icon.svg' alt='socials' />
                             <p className='socialText'> twitter | X</p>
                         </div>
                         <div className='socials'>
-                            <img src='/images/threads-icon.svg' alt='socials'/>
+                            <img src='/images/threads-icon.svg' alt='socials' />
                             <p className='socialText'>threads</p>
                         </div>
                         <div className='socials'>
-                            <img src='/images/tg-icon.svg' alt='socials'/>
+                            <img src='/images/tg-icon.svg' alt='socials' />
                             <p className='socialText'>telegram</p>
                         </div>
                     </div>
