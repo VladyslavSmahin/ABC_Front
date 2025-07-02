e31205501d06e86590f4bdfcf860eaedcc354cef
Vladyslav Smahin <vladislavsmagin1@gmail.com>
2025-07-02 16:39:30 +0300

    fix editor remove plugins

--- src/components/AdminEditor/TinyEditor.jsx
+++ src/components/AdminEditor/TinyEditor.jsx
@@ -6,22 +6,20 @@ export default function TinyEditor({ data, onChange }) {
 
     return (
         <Editor
-            apiKey="abfauxk3v1t5394p0odjbot6rzlkzaaq6vjrl4uzqdnjrcp3" // бесплатно
+            apiKey="abfauxk3v1t5394p0odjbot6rzlkzaaq6vjrl4uzqdnjrcp3" // бесплатный API-ключ
             onInit={(evt, editor) => (editorRef.current = editor)}
             value={data}
             init={{
                 height: 600,
                 menubar: 'file edit view insert format tools table help',
-                plugins: "advcode advlist advtable anchor autocorrect autolink autosave casechange charmap checklist codesample directionality editimage emoticons export footnotes formatpainter help image insertdatetime link linkchecker lists media mediaembed mergetags nonbreaking pagebreak permanentpen powerpaste searchreplace table tableofcontents tinymcespellchecker typography visualblocks visualchars wordcount",
-                toolbar: "undo redo spellcheckdialog  | blocks fontfamily fontsize | bold italic underline forecolor backcolor | link image | align lineheight checklist bullist numlist | indent outdent | removeformat typography",
+                plugins: "advlist anchor autolink autosave charmap codesample directionality help image insertdatetime link lists media nonbreaking pagebreak searchreplace table visualblocks visualchars wordcount",
+                toolbar: "undo redo | blocks fontfamily fontsize | bold italic underline forecolor backcolor | link image | align lineheight bullist numlist | indent outdent | removeformat",
                 font_size_formats: '8pt 10pt 12pt 14pt 18pt 24pt 36pt',
                 content_style: `
                     body { font-family:Helvetica,Arial,sans-serif; font-size:14px }
                     img { max-width: 100% !important; height: auto !important; display: block; }
                 `
             }}
-
-
             onEditorChange={(newContent) => {
                 onChange(newContent);
             }}
